7fa087036458ebe651af4e7e0b952e7786efdaf0
James <ahrensjc1@gcc.edu>
2019-02-02 19:11:26 -0500

    sets up basic tab view

---
+++ Studor/Studor/ExploreTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  ExploreTableViewCell.swift
+//  Studor
+//
+//  Created by James Ahrens on 2/2/19.
+//  Copyright © 2019 James Ahrens. All rights reserved.
+//
+
+import UIKit
+
+class ExploreTableViewCell: UITableViewCell {
+    
+    var thing = 0
+
+    @IBOutlet weak var connect: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
