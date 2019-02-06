f68e9cd052024f3e678ce0c42248d8f08e7d70b7
Seanboyy <sbamford12@gmail.com>
2019-02-05 22:13:58 -0500

    Added sign in / sign up page

---
+++ Studor/Studor/SignInSignUp.h
@@ -0,0 +1,17 @@
+//
+//  SignInSignUp.h
+//  Studor
+//
+//  Created by Sean Bamford on 2/5/19.
+//  Copyright © 2019 James Ahrens. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SignInSignUp : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Studor/Studor/SignInSignUp.swift
@@ -0,0 +1,9 @@
+//
+//  SignInSignUp.swift
+//  Studor
+//
+//  Created by Sean Bamford on 2/5/19.
+//  Copyright © 2019 James Ahrens. All rights reserved.
+//
+
+import Foundation
