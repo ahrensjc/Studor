bee6b27272c30363a1a803ab255afce3db3c7444
Seanboyy <sbamford12@gmail.com>
2019-02-07 16:27:24 -0500

    Added sign up screen

--- Studor/Studor/SignUp.swift
+++ Studor/Studor/SignUp.swift
@@ -6,11 +6,35 @@
 //  Copyright © 2019 James Ahrens. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
+class SignUp : UIViewController {
+    
+    @IBOutlet weak var emailBox : UITextBox?
+    
+    @IBOutlet weak var usernameBox : UITextBox?
+    
+    @IBOutlet weak var passwordBox : UITextBox?
+    
+    @IBOutlet weak var confirmPasswordBox : UITextBox?
+    
+    @IBOutlet weak var accountType : UISegmentedControl?
+
+    override func viewDidLoad(){
+        super.viewDidLoad()
+    }
+    
+    @IBAction func tapSignUp(_ sender: Any){
+        if emailBox!.text!.count > 0 && usernameBox!.text!.count > 0 && passwordBox!.text!.count > 0 && confirmPasswordBox!.text!.count > 0 && passwordBox!.text == confirmPasswordBox!.text {
+            //do sign up
+        }
+    }
+    
 /*
  Auth.auth().createUser(withEmail: email, password: password) { (authResult, error) in
 
     guard let user = authResult?.user else { return }
  }
  */
+
+}
