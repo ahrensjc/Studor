aeb1e2dac4e679a7c18bb2755c67fd5fb3e8bf04
Tyler Fehr <fehrtj1@gcc.edu>
2019-02-07 16:53:24 -0500

    Added dummies for sign in/ signup stuff

--- Studor/Studor/SignUp.swift
+++ Studor/Studor/SignUp.swift
@@ -9,8 +9,33 @@
 import Foundation
 
 /*
+ 
+ if let email = emailTextField, let password = passwordTextField.text, let passwordConfirm = passwordConfirmTextField.text {
+    if !email.endsWith("gcc.edu) {
+    showMessagePrompt(withString: "Studor is restricted to GCC students only", title: "Error")
+    return
+    }
+ if password != passwordConfirm {
+    showMessagePrompt(withString: "Password fields do not match", title: "Error")
+    return
+ }
+ if password.count < 7 {
+    showMessagePrompt(withString: "Password must be at least 7 characters", title: "Error")
+ }
+ }
+ 
  Auth.auth().createUser(withEmail: email, password: password) { (authResult, error) in
 
     guard let user = authResult?.user else { return }
+ }
+ 
+ func showMessagePrompt(withString: String, title: String){
+ let alert = UIAlertController(title: title, message: withString, preferredStyle: UIAlertController.Style.alert)
+ 
+ // add an action (button)
+ alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+ 
+ // show the alert
+ self.present(alert, animated: true, completion: nil)
  }
  */
