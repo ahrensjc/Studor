100b4d0678ef1c1058a16e6c2a321d469bdb544a
James <ahrensjc1@gcc.edu>
2019-02-07 21:33:53 -0500

    Schedule view setup

---
+++ Studor/Studor/ScheduleTableViewCell.swift
@@ -0,0 +1,39 @@
+//
+//  ScheduleTableViewCell.swift
+//  Studor
+//
+//  Created by James Ahrens on 2/7/19.
+//  Copyright © 2019 James Ahrens. All rights reserved.
+//
+
+import UIKit
+
+class ScheduleTableViewCell: UITableViewCell {
+    
+    
+    @IBOutlet weak var eventLabel: UILabel!
+    
+        // TODO
+        // send filler event data to database
+        //
+        // for sprint 2:
+        // redo this entire part so it isn't on the table
+        // go to modal/new page to fill out event info
+        // get rid of this function
+    
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        
+        
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
