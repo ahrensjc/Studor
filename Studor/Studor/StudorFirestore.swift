7ba9959f2b9c187fb1396e9a07b87bd30a9f60cc
Tyler Fehr <fehrtj1@gcc.edu>
2019-02-10 17:37:14 -0500

    User account documents are now implemented.

---
+++ Studor/Studor/StudorFirestore.swift
@@ -0,0 +1,13 @@
+//
+//  StudorFirestore.swift
+//  Studor
+//
+//  Created by Tyler Fehr on 2/10/19.
+//  Copyright © 2019 James Ahrens. All rights reserved.
+//
+
+import Foundation
+
+class Commands {
+    
+}
