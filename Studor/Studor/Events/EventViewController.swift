210d3c065cfed3f4a4510f194f1961a3fa643313
James <ahrensjc1@gcc.edu>
2019-03-14 23:33:07 -0400

    added event viewer vc

---
+++ Studor/Studor/Events/EventViewController.swift
@@ -0,0 +1,36 @@
+//
+//  EventViewController.swift
+//  Studor
+//
+//  Created by James Ahrens on 3/14/19.
+//  Copyright © 2019 James Ahrens. All rights reserved.
+//
+
+import UIKit
+
+class EventViewController: UIViewController {
+
+    @IBOutlet weak var eventNameLabel: UILabel!
+    @IBOutlet weak var eventDateLabel: UILabel!
+    @IBOutlet weak var eventLocationLabel: UILabel!
+    @IBOutlet weak var eventOwnerLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
