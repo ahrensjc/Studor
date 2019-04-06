79b83025c4ca2349b34af359f4cea8fc3a3fd0a2
JJ Swar <Dr.jabree@gmail.com>
2019-04-05 21:31:32 -0400

    Started the display member process

---
+++ Studor/Studor/ViewGroupProfileViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewGroupProfileViewController.swift
+//  Studor
+//
+//  Created by JJ 2 on 4/5/19.
+//  Copyright © 2019 James Ahrens. All rights reserved.
+//
+
+import UIKit
+import FirebaseDatabase
+import Firebase
+import SendBirdSDK
+
+class ViewGroupProfileViewController: UIViewController {
+    
+    @IBOutlet weak var memberList: UILabel?
+    
+    var channelName: String?
+    
+    var sendbirdID: String?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+
+}
