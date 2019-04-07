1a181e17a8138d25e688ba0cebd13778204abc76
James <ahrensjc1@gcc.edu>
2019-04-07 13:17:33 -0400

    invite users to group
    still need to test this

--- Studor/Studor/ViewGroupProfileViewController.swift
+++ Studor/Studor/ViewGroupProfileViewController.swift
@@ -10,20 +10,46 @@ import UIKit
 import FirebaseDatabase
 import Firebase
 import SendBirdSDK
+import iOSDropDown
 
 class ViewGroupProfileViewController: UIViewController {
     
     @IBOutlet weak var groupName: UILabel!
     @IBOutlet weak var memberList: UITextView?
+    @IBOutlet weak var inviteUsersDropDown: DropDown!
+    @IBOutlet weak var inviteUsersLabel: UILabel!
+    @IBOutlet weak var inviteButton: UIButton!
     
     var channelName: String?
     var channelUrl: String?
     var sendbirdID: String?
+    var usersList : [SBDUser]!
+    var channel: SBDGroupChannel!
     
     var channelMembers: [SBDUser]!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let applicationUserListQuery = SBDMain.createApplicationUserListQuery()
+        //applicationUserListQuery?.userIdsFilter = ["jim"]
+        applicationUserListQuery?.limit = 100
+        applicationUserListQuery?.loadNextPage(completionHandler: { (users, error) in
+            guard error == nil else {
+                print("error getting user list")
+                print(error as Any)
+                return
+            }
+            self.usersList = users!
+            var usernames = [String]()
+            for user in users! {
+                usernames.append(user.userId)
+            }
+            self.inviteUsersDropDown.optionArray = usernames
+        })
+        
+        inviteUsersDropDown.listHeight = 120
+        inviteUsersDropDown.selectedRowColor = UIColor.white
         
         self.groupName.layer.borderWidth = 1.5; //this is the width of the border of nickname om profile page
         self.groupName.layer.cornerRadius = 8; //rounded edges
@@ -37,15 +63,17 @@ class ViewGroupProfileViewController: UIViewController {
             }
             
             self.groupName.text = groupChannel?.name
-            
             self.channelMembers = (groupChannel!.members as! [SBDUser])
-            
             self.memberList!.text = ""
             
             for member in self.channelMembers {
                 let append =  "\n" + member.nickname!
                 self.memberList!.text.append(append)
-                
+                if member.userId == firebaseSingleton.getSendbirdId() {
+                    self.inviteButton.isHidden = false
+                    self.inviteUsersLabel.isHidden = false
+                    self.inviteUsersDropDown.isHidden = false
+                }
             }
             
             
@@ -53,6 +81,22 @@ class ViewGroupProfileViewController: UIViewController {
         }
       
     }
+    
+    @IBAction func inviteUserButtonTapped(_ sender: Any) {
+        if(inviteUsersDropDown.text != "" || inviteUsersDropDown != nil) {
+            //participants.append(addParticipantsDropDown.text!)
+            //participantsTextView.text.append(", " + addParticipantsDropDown.text!)
+            inviteUsersDropDown.text = ""
+            channel.inviteUserIds([inviteUsersDropDown.text!]) { (error) in
+                guard error == nil else {   // Error.
+                    return
+                }
+                
+                // ...
+            }
+        }
+    }
+    
 }
     
     
