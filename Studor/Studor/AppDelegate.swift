1b8aed61d281e37b92e7a4b502de511365542255
James <ahrensjc1@gcc.edu>
2019-04-07 20:12:30 -0400

    Revert "Revert "Merge branch 'master' of https://github.com/ahrensjc/Studor""
    This reverts commit d2e5c00c1703a541fa8b91f92f73b323f494d82c.

--- Studor/Studor/AppDelegate.swift
+++ Studor/Studor/AppDelegate.swift
@@ -8,15 +8,31 @@
 
 import UIKit
 import Firebase
+import UserNotifications
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate {
 
     var window: UIWindow?
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         FirebaseApp.configure()
+        
+        let center = UNUserNotificationCenter.current()
+        let category = UNNotificationCategory(identifier: "Note", actions: [], intentIdentifiers: [], options: [])
+        center.setNotificationCategories([category])
+        
+        let options: UNAuthorizationOptions = [.alert, .sound]
+        center.requestAuthorization(options: options, completionHandler: {    (granted, error) in    if !granted {        print("Bad things have happened")    }})
+        
+        center.delegate = self
+        
         return true
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        print(notification.request.content.body)
+        completionHandler([.alert])
     }
 }
 
