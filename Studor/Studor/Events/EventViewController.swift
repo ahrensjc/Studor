2b0cbfcfd2d4d8636e2749dacd89054ad6ff442b
Tyler Fehr <fehrtj1@gcc.edu>
2019-04-10 14:11:49 -0400

    Attempt to refresh events from confirm button

--- Studor/Studor/Events/EventViewController.swift
+++ Studor/Studor/Events/EventViewController.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2019 James Ahrens. All rights reserved.
 //
 
+import Firebase
 import UIKit
 
 class EventViewController: UIViewController {
@@ -18,6 +19,7 @@ class EventViewController: UIViewController {
     
     var selectedEvent: Event?
     var timeOfEvent: String?
+    let db = Firestore.firestore()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -39,6 +41,38 @@ class EventViewController: UIViewController {
             participantsTextView.text! += p + "\n"
             
         }
+        
+    }
+    
+    func reloadEventDataAfterEdit(id: String, completion: @escaping (Event?, String?) -> Void){
+        
+        let ref = db.collection("Events").document(id).getDocument{ (document, error) in
+            if let document = document, document.exists {
+                let dataDescription = document.data().map(String.init(describing:)) ?? "nil"
+                
+                let newDate = Date(timeIntervalSince1970: TimeInterval((document.data()!["date"] as! Timestamp).seconds))
+                let newTitle = document.data()!["title"] as! String
+                let newLoc = document.data()!["location"] as! String
+                
+                let editedEvent = Event(date: newDate, title: newTitle, participants: self.selectedEvent!.participants!, eventId: self.selectedEvent!.eventId!, creator: self.selectedEvent!.creator!, loc: newLoc)
+                
+                completion(editedEvent, nil)
+                
+            } else {
+                print("Could not retrieve event data after edit")
+                completion(nil, "Could not retrieve event data after edit")
+            }
+        }
+    }
+    
+    @IBAction func unwindToEventViewAndReload(segue: UIStoryboardSegue){
+        
+        print("unwind and reload")
+        
+        self.reloadEventDataAfterEdit(id: self.selectedEvent!.eventId!) { (editedEvent, error) in
+            self.selectedEvent = editedEvent
+        }
+        self.setSelectedEventElements()
         
     }
     
